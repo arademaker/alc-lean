a779e5ddb775bbbeebca75bc8293f6bd05ae0860
Alexandre Rademaker <arademaker@gmail.com>
2021-02-10 09:42:19 -0300

    testing atoms as strings

--- src/alc.lean
+++ src/alc.lean
@@ -1,21 +1,31 @@
+
 import data.set 
 open set
 
-
 namespace ALC
 
-inductive Role (AtomicRole : Type) : Type
+structure AtomicRole :=
+ (id : string)
+
+structure AtomicConcept := 
+ (id : string)
+
+#check { AtomicRole . id := "hasChild"}
+
+#check ({ id := "hasChild" } : AtomicRole)
+
+inductive Role : Type
   | Atomic : AtomicRole → Role
 
-inductive Concept (AtomicConcept AtomicRole : Type) : Type 
+inductive Concept : Type 
   | TopConcept    : Concept
   | BottomConcept : Concept
   | Atomic        : AtomicConcept → Concept
   | Negation      : Concept → Concept
   | Intersection  : Concept → Concept → Concept
   | Union         : Concept → Concept → Concept
-  | Some          : Role AtomicRole → Concept → Concept
-  | Every         : Role AtomicRole → Concept → Concept
+  | Some          : Role → Concept → Concept
+  | Every         : Role → Concept → Concept
 
 open Concept Role
 
@@ -28,7 +38,7 @@ open Concept Role
 -- notation `Only` R . C := Concept.Al R C 
 
 -- interpretation structure 
-structure Interpretation (AtomicConcept AtomicRole : Type) := 
+structure Interpretation := 
 mk :: (δ : Type) 
       (atom_C : AtomicConcept → set δ)
       (atom_R : AtomicRole → set (δ × δ))
@@ -36,13 +46,11 @@ mk :: (δ : Type)
 variables {AtomicConcept AtomicRole : Type}
 
 -- role interpretation
-definition r_interp {I : Interpretation AtomicConcept AtomicRole} : 
- Role AtomicRole → set (I.δ × I.δ)  
+definition r_interp (I : Interpretation) : Role → set (I.δ × I.δ)  
   | (Role.Atomic R) := I.atom_R R
 
 -- concept interpretation
-definition interp {I : Interpretation AtomicConcept AtomicRole} : 
-  Concept AtomicConcept AtomicRole → set I.δ 
+definition interp (I : Interpretation) : Concept → set I.δ 
  | TopConcept           := univ
  | BottomConcept        := ∅ 
  | (Atomic C)           := I.atom_C C
@@ -50,9 +58,9 @@ definition interp {I : Interpretation AtomicConcept AtomicRole} :
  | (Intersection C1 C2) := (interp C1) ∩ (interp C2)
  | (Union C1 C2)        := (interp C1) ∪ (interp C2)
  | (Some R C)           := { a: I.δ | ∃ b : I.δ, 
-                            (a, b) ∈ (@r_interp _ _ I R) ∧ b ∈ (interp C) }
+                            (a, b) ∈ (r_interp I R) ∧ b ∈ (interp C) }
  | (Every R C)          := { a: I.δ | ∀ b : I.δ,
-                            (a, b) ∈ (@r_interp _ _ I R) → b ∈ (interp C) }
+                            (a, b) ∈ (r_interp I R) → b ∈ (interp C) }
 
 end ALC
 
@@ -62,40 +70,50 @@ namespace test
 open ALC
 open ALC.Concept
 
-inductive ac : Type
- | man : ac
- | woman : ac
 
-inductive ar : Type
- | hasChild : ar
+@[reducible]
+def ic : AtomicConcept → set ℕ  
+ | { id := "man" }   := ({2,4} : set ℕ)
+ | { id := "woman" } := ({1,3} : set ℕ)     
+ | _ := ∅ 
 
-open ac 
-open ar
+@[reducible]
+def ir : AtomicRole → set (ℕ × ℕ)
+ | { id := "hasChild" } := ({(1,2),(4,3)} : set (ℕ × ℕ))
+ | _ := ∅ 
 
-def ic : ac → set ℕ  
- | man   := ({2,4} : set ℕ)
- | woman := ({1,3} : set ℕ)     
-
-def ir : ar → set (ℕ × ℕ)
- | hasChild := ({(1,2),(4,3)} : set (ℕ × ℕ))
-
+@[reducible]
 def i := Interpretation.mk ℕ ic ir
 
+#check Concept.Atomic { id := "man" }
 
-#check Concept.Atomic man 
-
+#reduce interp i (Concept.Atomic { id := "man"})
 
 -- ∀ hasChild.man (the concept of all things such that all its fillers
 -- for the role 'hasChild' are of type 'man')
 
-#reduce @interp ac ar i (Every (Role.Atomic hasChild) (Concept.Atomic man))
 
-#reduce interp (Every (Role.Atomic hasChild) (Concept.Atomic man))
+#reduce interp i (Every (Role.Atomic { id := "hasChild"}) (Concept.Atomic { id := "man"}))
+
+
+example : 
+ interp i (Every (Role.Atomic {id := "hasChild"}) (Concept.Atomic {id := "man"})) = ({1} : set ℕ) :=
+begin
+ ext n,  
+ apply iff.intro,
+ intro h1, 
+ dsimp [interp,r_interp,i] at h1, 
+ have h2 := h1 1,
+ 
+end
+
 
 end test
 
+
 /- References:
 
-- DL Prime: https://arxiv.org/abs/1201.4089v3
+DL Prime: https://arxiv.org/abs/1201.4089v3
+
 -/
 

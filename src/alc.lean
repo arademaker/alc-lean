431d3daa80fd5e12c5c891a42d8a0b6fa6993b11
Alexandre Rademaker <arademaker@gmail.com>
2021-02-10 18:52:11 -0300

    some more proofs

--- src/alc.lean
+++ src/alc.lean
@@ -83,13 +83,16 @@ inductive ac : Type
 inductive ar : Type
  | hasChild : ar
 
+@[reducible]
 def ic : ac → set ℕ  
  | ac.man   := ({2,4} : set ℕ)
  | ac.woman := ({1,3} : set ℕ)     
 
+@[reducible]
 def ir : ar → set (ℕ × ℕ)
  | ar.hasChild := ({(1,2),(4,3)} : set (ℕ × ℕ))
 
+@[reducible]
 def i := Interpretation.mk ℕ ic ir
 
 
@@ -126,9 +129,58 @@ begin
  } 
 end
 
-example : 
- interp i (Every (Role.Atomic ar.hasChild) (Concept.Atomic ac.man)) = ({1,2,3} : set ℕ) := sorry
+-- Mario's proof
+example :
+  interp i (Every (Role.Atomic ar.hasChild) (Concept.Atomic ac.man)) = ({4}ᶜ : set ℕ) :=
+begin
+  ext n,
+  simp [interp, r_interp, ir, ic],
+  split,
+  { rintro h rfl,
+    have := h 3, revert this,
+    norm_num, 
+  },
+  { rintro h _ (⟨rfl, rfl⟩|⟨rfl, rfl⟩), {norm_num},
+    cases h rfl },
+end
 
+-- a detailed proof
+example :
+  interp i (Every (Role.Atomic ar.hasChild) (Concept.Atomic ac.man)) = ({4}ᶜ : set ℕ) :=
+begin
+  ext n,
+  simp [interp, r_interp, ir, ic],
+  split,
+  { rintro h rfl,
+    have := h 3, revert this,
+    norm_num, 
+  },
+  { intros h1 a h2,
+    cases h2 with h2a h2b,
+    exact or.inl h2a.2,
+    exfalso, exact h1 h2b.left,
+  },
+end
+
+-- if i, ic and ir are not 'reducible'
+example :
+  interp i (Every (Role.Atomic ar.hasChild) (Concept.Atomic ac.man)) = ({4}ᶜ : set ℕ) :=
+begin
+  dsimp [i, interp],
+  ext n,
+  simp [r_interp, ir],
+  split,
+  { rintro H rfl,
+    have := H 3, revert this,
+    norm_num },
+  { rintro h _ (⟨rfl, rfl⟩|⟨rfl, rfl⟩), {norm_num},
+    cases h rfl },
+end
+
+
+example : 
+ interp i (Every (Role.Atomic ar.hasChild) (Concept.Atomic ac.man)) = ({1,2,3} : set ℕ)  
+  := sorry
 
 -- more general properties of 'interp' should also be provable:
 
@@ -138,7 +190,6 @@ example (a b : Type) (i : Interpretation a b) (c : Concept a b) :
 
 example (a b : Type) (i : Interpretation a b) (c : Concept a b) : 
   interp i (Union c (Negation c)) = univ := sorry
-
 
 
 -- detailed proofs for the steps closed with 'finish' above.

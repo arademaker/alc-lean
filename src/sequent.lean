1003ab75480613cbc8c8524d9c9b50cfe42c21cd
Joao Paulo <jpbregunci@gmail.com>
2021-03-24 17:31:22 -0300

    Few Changes

--- src/sequent.lean
+++ src/sequent.lean
@@ -1,4 +1,5 @@
 import alc
+import data.multiset
 
 open ALC
 open ALC.Concept
@@ -23,15 +24,30 @@ mk :: (roles : list (upper_Label AC AR))
 
 #check (lab_Concept.mk [@upper_Label.Negation α β] (@Concept.Bot α β )).roles.head
 
-#reduce list.ilast [1,2,3]
-
 def sigma_line {AC AR : Type} : lab_Concept AC AR → upper_Label AC AR →  lab_Concept AC AR
-  | a upper_Label.Negation        := lab_Concept.mk (a.roles.tail) (Concept.Negation a.concept)
   | a upper_Label.Empty           := a
+  | a upper_Label.Negation        := lab_Concept.mk (a.roles.tail) (Concept.Negation a.concept)
   | a (upper_Label.Forall RO CO)  := lab_Concept.mk (a.roles.tail) (Concept.Every RO a.concept)
   | a (upper_Label.Exist RO CO)   := lab_Concept.mk (a.roles.tail) (Concept.Some RO a.concept)
+
 
 def sigma_apply {AC AR : Type} (lc : @lab_Concept AC AR) :=
   sigma_line lc (lc.roles.head)
 
+def sigma_line2 {AC AR : Type} : list (upper_Label AC AR) → lab_Concept AC AR → lab_Concept AC AR
+  | [] a := a
+  | (_::ls) a := sigma_line2 (ls) (sigma_apply a)
+
+def sigma_exhaust {AC AR : Type} (lb : lab_Concept AC AR) :=
+  lab_Concept.concept (sigma_line2 (lab_Concept.roles lb) lb)
+
 #reduce lab_Concept.concept (sigma_apply (lab_Concept.mk [@upper_Label.Negation α β] (@Concept.Bot α β )))
+
+#reduce sigma_exhaust (lab_Concept.mk [@upper_Label.Negation α β] (@Concept.Bot α β ))
+
+#check multiset.fold (Concept.Intersection) Concept.Top {Concept.Top}
+
+def subsequent {AC AR : Type} (as : multiset (lab_Concept AC AR)) (bs : multiset (Concept AC AR)) :=
+  multiset.fold (@Concept.Intersection AC AR) (@Concept.Top AC AR) (multiset.map sigma_exhaust as)
+
+#check and.comm

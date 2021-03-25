dfce9898e8de64fb5c560d18854d7ef451a47779
Joao Paulo <jpbregunci@gmail.com>
2021-03-25 13:07:52 -0300

    Prototype

--- src/sequent.lean
+++ src/sequent.lean
@@ -18,9 +18,15 @@ inductive Label (AC AR : Type) : Type
 instance Label_inhabited {AC AR : Type} : 
   inhabited (Label AC AR) := inhabited.mk Label.Empty
 
+instance Concept_inhabited {AC AR : Type} :
+  inhabited (Concept AC AR) := inhabited.mk Concept.Top
+
 structure LConcept (AC AR: Type) :=
 mk :: (roles : list (Label AC AR))
       (concept : Concept AC AR)
+
+instance LConcept_inhabited {AC AR : Type} :
+  inhabited (LConcept AC AR) := inhabited.mk (LConcept.mk [Label.Empty] Concept.Top)
 
 #check (LConcept.mk [@Label.Negation α β] (@Concept.Bot α β )).roles.head
 
@@ -47,11 +53,33 @@ def sigma_exhaust {AC AR : Type} (lb : LConcept AC AR) :=
 
 #check multiset.fold (Concept.Intersection) Concept.Top {Concept.Top}
 
-#check Concept.rec
-instance Concept_Intersection_is_commutative {AC AR : Type}:
-  @is_commutative (Concept AC AR) Concept.Intersection := ⟨λ a b, by⟩
+--#check Concept.rec
+--instance Concept_Intersection_is_commutative {AC AR : Type}:
+--  @is_commutative (Concept AC AR) Concept.Intersection := ⟨λ a b, by⟩
 
-def subsequent {AC AR : Type} (as : multiset (LConcept AC AR)) (bs : multiset (Concept AC AR)) :=
-  multiset.fold (@Concept.Intersection AC AR) (@Concept.Top AC AR) (multiset.map sigma_exhaust as)
+--this is the idea for the implementation, we can use folds in multiset, but I still have to prove commutative to
+--use it, but I am having trouble as it is...
 
-#check and.comm
+def inter_LConcepts {AC AR : Type} (as : list (LConcept AC AR)) :=
+  list.foldl (Concept.Intersection) (sigma_exhaust (list.head as)) (list.map sigma_exhaust (list.tail as))
+
+def union_LConcepts {AC AR : Type} (as : list (LConcept AC AR)) :=
+  list.foldl (Concept.Union) (sigma_exhaust (list.head as)) (list.map sigma_exhaust (list.tail as))
+
+def subsequent {AC AR : Type} (as : list (LConcept AC AR)) (bs : list (LConcept AC AR)) :=
+  subsumption (inter_LConcepts as) (union_LConcepts bs)
+
+#reduce list.foldl (λ x y, Concept.Intersection x y) (Concept.Top) [Concept.Bot]
+
+
+#reduce 1 ∈ [1,2,3]
+
+lemma weak_l_srule {AC AR : Type} {as bs : list (LConcept AC AR)} 
+  (a : LConcept AC AR) (h1 : list.mem a as) (h : subsequent as bs) : subsequent (a::as) bs :=
+begin
+  unfold subsequent at *,
+  unfold subsumption at *,
+  intro I,
+  have hn : interp I (inter_LConcepts as) ⊆ interp I (union_LConcepts bs), from h I,
+  
+end 
